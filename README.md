c3a5fca779aafb7208ce9b786fcbf2fa8980e10e
coolguy284 <coolguy284v64@gmail.com>
2022-08-18 00:31:01 -0400

    update readme, command help, start impl readdir
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Hash Backup Tool
 
-This program is used to create backups of a given folder tree by referencing each file by its hash. Each additional backup to the same backup dir will only add new files that have a different hash, so space is conserved. All file timestamps made available in node.js are preserved.
+This program is used to create backups of a given folder tree by referencing each file by its hash. Each additional backup to the same backup dir will only add new files that have a different hash, so space is conserved. All file and folder timestamps (made available in node.js) are preserved, and so are empty folders.
 
 # Usage
 
@@ -11,15 +11,13 @@ Download `hash_backup.js` and place anywhere. Run `node hash_backup.js` to see a
 ```
 Node Hash Backup Tool
 
-Usage: node hash_backup.js [command] [arguments]
+Usage: node hash_backup.js [command] [options]
 
 Command `init`:
-  Usage: node hash_backup.js init [options]
-  
   Initalizes empty hash backup in backup dir.
   
   Options:
-    --to <backupDir> (default `.`): The backup dir to initialize.
+    --to <backupDir> (required): The hash backup dir to initialize.
     --hash <algorythm> (default `sha384`): The hash algorythm to use on the files.
     --hash-slice-length (default `2`): The length of the hash slice used to split files into folders.
     --hash-slices (default `2`): The number of nested subfolders of hash slices each file should be under.
@@ -27,10 +25,27 @@ Command `init`:
     --compress-level (default 6): The amount to compress files (valid is 1 through 9).
 
 Command `delete`:
-  Usage: node hash_backup.js delete [options]
-  
   Removes all files at hash backup dir.
   
   Options:
-    --to <backupDir> (default .): The backup dir to remove contents of.
+    --to <backupDir> (required): The hash backup dir to remove contents of.
+
+Command `backup`:
+  Backs up a folder to the hash backup.
+  
+  Options:
+    --from <path> (required): The directory to backup.
+    --to <backupDir> (required): The hash backup folder to use.
+    --name <name> (required): The name of the backup.
+    --ignore-symlinks <value> (default false): If true, symlinks will be ignored. If false, symlinks will be copied over as regular files (and the modtime of the destination file will be used).
+    --in-memory <value> (default true): Read file into memory and store hash and compressed forms into memory. Minimizes hard drive reads/writes. Turn off for files too large to fit in memory.
+
+Command `restore`:
+  Restores a folder from the hash backup.
+  
+  Options:
+    --from <backupDir> (required): The hash backup folder to use.
+    --to <path> (required): The directory to restore to.
+    --name <name> (required): The name of the backup.
+    --verify <value> (default true): If true, file checksums will be verified as they are copied out.
 ```
--- src/hash_backup.js
+++ src/hash_backup.js
@@ -22,7 +22,7 @@ async function getUserInput() {
   return choice;
 }
 
-async function getAllFilesInDir(path, excludeDirs) {
+async function getAllEntriesInDir(path, excludeDirs) {
   if (!Array.isArray(excludeDirs)) excludeDirs = [];
   
   var entries = [];
@@ -86,6 +86,8 @@ async function initBackupDir(opts) {
   
   await fs.promises.mkdir(path.join(backupDir, 'files_meta'));
   
+  await fs.promises.mkdir(path.join(backupDir, 'backups'));
+  
   await fs.promises.writeFile(path.join(backupDir, 'info.json'), JSON.stringify({
     version: 1,
     hash,
@@ -120,15 +122,43 @@ async function runIfMain() {
     console.log(
       'Node Hash Backup Tool\n' +
       '\n' +
-      'Usage: node hash_backup.js [command] [arguments]\n' +
+      'Usage: node hash_backup.js [command] [options]\n' +
       '\n' +
       'Command `init`:\n' +
-      '  Usage: node hash_backup.js init <backupDir>\n' +
       '  Initalizes empty hash backup in backup dir.\n' +
+      '  \n' +
+      '  Options:\n' +
+      '    --to <backupDir> (required): The hash backup dir to initialize.\n' +
+      '    --hash <algorythm> (default `sha384`): The hash algorythm to use on the files.\n' +
+      '    --hash-slice-length (default `2`): The length of the hash slice used to split files into folders.\n' +
+      '    --hash-slices (default `2`): The number of nested subfolders of hash slices each file should be under.\n' +
+      '    --compress-algo (default `brotli`): The algorythm to compress files (`none` for no algo).\n' +
+      '    --compress-level (default 6): The amount to compress files (valid is 1 through 9).\n' +
       '\n' +
       'Command `delete`:\n' +
-      '  Usage: node hash_backup.js delete <backupDir>\n' +
-      '  Removes hash backup at backup dir.'
+      '  Removes all files at hash backup dir.\n' +
+      '  \n' +
+      '  Options:\n' +
+      '    --to <backupDir> (required): The hash backup dir to remove contents of.\n' +
+      '\n' +
+      'Command `backup`:\n' +
+      '  Backs up a folder to the hash backup.\n' +
+      '  \n' +
+      '  Options:\n' +
+      '    --from <path> (required): The directory to backup.\n' +
+      '    --to <backupDir> (required): The hash backup folder to use.\n' +
+      '    --name <name> (required): The name of the backup.\n' +
+      '    --ignore-symlinks <value> (default false): If true, symlinks will be ignored. If false, symlinks will be copied over as regular files (and the modtime of the destination file will be used).\n' +
+      '    --in-memory <value> (default true): Read file into memory and store hash and compressed forms into memory. Minimizes hard drive reads/writes. Turn off for files too large to fit in memory.\n' +
+      '\n' +
+      'Command `restore`:\n' +
+      '  Restores a folder from the hash backup.\n' +
+      '  \n' +
+      '  Options:\n' +
+      '    --from <backupDir> (required): The hash backup folder to use.\n' +
+      '    --to <path> (required): The directory to restore to.\n' +
+      '    --name <name> (required): The name of the backup.\n' +
+      '    --verify <value> (default true): If true, file checksums will be verified as they are copied out.'
     );
   } else {
     let commandArgsRaw = argvSliced.slice(1);
@@ -218,7 +248,7 @@ async function runIfMain() {
 
 module.exports = exports = {
   _checkBackupDirIsDir,
-  getUserInput, getAllFilesInDir,
+  getUserInput, getAllEntriesInDir,
   initBackupDir, deleteBackupDir,
   runIfMain,
 };
