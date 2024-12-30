2f92dc0ef84cb50f59a4e36a908d1de30ed7f448
coolguy284 <coolguy284v64@gmail.com>
2024-12-30 09:20:13 -0500

    create recursive readdir function
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- new-src/lib/fs.mjs
+++ new-src/lib/fs.mjs
@@ -1,13 +1,26 @@
 import {
   access,
+  lstat,
   open,
+  readdir,
   rename,
   stat,
   writeFile,
 } from 'fs/promises';
+import {
+  join,
+  sep,
+} from 'path';
+
+import { Enum } from './enum.mjs';
 
 const TEMP_NEW_FILE_SUFFIX = '_new';
 const LARGE_FILE_CHUNK_SIZE = 4 * 2 ** 20;
+export const SymlinkModes = Enum([
+  'IGNORE',
+  'PASSTHROUGH',
+  'PRESERVE',
+]);
 
 export async function errorIfPathNotDir(validationPath) {
   if (typeof validationPath != 'string') {
@@ -65,4 +78,154 @@ export async function fileExists(filename) {
   } catch {
     return false;
   }
+}
+
+async function recursiveReaddirInternal(
+  fileOrDirPath,
+  {
+    excludedFilesOrDirs,
+    symlinkMode,
+  }
+) {
+  let selfStats;
+  
+  switch (symlinkMode) {
+    case SymlinkModes.IGNORE: {
+      const stats = await lstat(fileOrDirPath);
+      
+      if (stats.isSymbolicLink()) {
+        return null;
+      } else {
+        selfStats = stats;
+      }
+      break;
+    }
+    
+    case SymlinkModes.PASSTHROUGH:
+      selfStats = await stat(fileOrDirPath);
+      break;
+    
+    case SymlinkModes.PRESERVE:
+      selfStats = await lstat(fileOrDirPath);
+      break;
+    
+    default:
+      throw new Error(`default case not possible: ${symlinkMode}`);
+  }
+  
+  let result = [
+    {
+      path: fileOrDirPath,
+      stats: selfStats,
+    }
+  ];
+  
+  if (selfStats.isDirectory()) {
+    const dirContents =
+      (await Promise.all(
+        (await readdir(dirPath))
+          .map(name => {
+            return {
+              name,
+              subExcludedFilesOrDirs:
+                excludedFilesOrDirs
+                  .filter(excludePath => {
+                    const [ first, ..._ ] = excludePath;
+                    return first == name;
+                  })
+                  .map(excludePath => excludePath.slice(1))
+            };
+          })
+          .filter(({ subExcludedFilesOrDirs }) =>
+            !subExcludedFilesOrDirs.some(excludePath => excludePath.length == 0)
+          )
+          .map(async ({ name, subExcludedFilesOrDirs }) => {
+            await recursiveReaddirInternal(
+              join(fileOrDirPath, name),
+              {
+                excludedFilesOrDirs: subExcludedFilesOrDirs,
+                symlinkMode,
+              }
+            );
+          })
+      ))
+      .filter(entry => entry != null)
+      .flat();
+    
+    dirContents.forEach(entry => result.push(entry));
+  }
+  
+  return result;
+}
+
+export async function recursiveReaddir(
+  dirPath,
+  {
+    excludedFilesOrDirs = [],
+    includeDirs = true,
+    entries = true,
+    symlinkMode = SymlinkModes.PRESERVE,
+  }
+) {
+  if (typeof dirPath != 'string') {
+    throw new Error(`dirPath not string: ${typeof dirPath}`);
+  }
+  
+  if (!Array.isArray(excludedFilesOrDirs)) {
+    throw new Error(`excludedFilesOrDirs not array: ${excludedFilesOrDirs}`);
+  }
+  
+  for (let i = 0; i < excludedFilesOrDirs.length; i++) {
+    if (typeof excludedFilesOrDirs[i] != 'string') {
+      throw new Error(`excludedFilesOrDirs[${i}] not string: ${typeof excludedFilesOrDirs[i]}`);
+    }
+  }
+  
+  if (sep == '\\') {
+    excludedFilesOrDirs =
+      excludedFilesOrDirs
+        .map(excludeEntry => excludeEntry.split(/\/|\\/));
+  } else {
+    excludedFilesOrDirs =
+      excludedFilesOrDirs
+        .map(excludeEntry => excludeEntry.split(sep));
+  }
+  
+  if (typeof includeDirs != 'boolean') {
+    throw new Error(`includeDirs not boolean: ${typeof includeDirs}`);
+  }
+  
+  if (typeof entries != 'boolean') {
+    throw new Error(`entries not boolean: ${typeof entries}`);
+  }
+  
+  if (typeof symlinkMode != 'string') {
+    throw new Error(`symlinkMode not string: ${typeof symlinkMode}`);
+  }
+  
+  if (!(symlinkMode in SymlinkModes)) {
+    throw new Error(`symlinkMode not in SymlinkModes: ${symlinkMode}`);
+  }
+  
+  const internalResult = await recursiveReaddirInternal(
+    dirPath,
+    {
+      excludedFilesOrDirs,
+      symlinkMode,
+    }
+  );
+  
+  return internalResult
+    .map(({ path, stats }) => {
+      if (!includeDirs && stats.isDirectory()) {
+        return null;
+      }
+      
+      if (!entries) {
+        return path;
+      } else {
+        return { path, stats };
+      }
+    })
+    .filter(entry => entry != null);
 }
