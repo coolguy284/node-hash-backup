74550066b7d27c360f89c5af5322c718bb023f52
coolguy284 <coolguy284v64@gmail.com>
2025-01-03 17:26:31 -0500

    fix deep clone func
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- src/lib/deep_clone.mjs
+++ src/lib/deep_clone.mjs
@@ -1,13 +1,13 @@
 // does not support self-referential objects
 export function deepObjectClone(object) {
-  if (typeof object != 'object') {
-    throw new Error(`object not object: ${typeof object}`);
+  if (typeof object != 'object' || object == null) {
+    throw new Error(`object not object: ${object == null ? 'null' : typeof object}`);
   }
   
   return Object.fromEntries(
     Object.entries(object)
       .map(([ key, value ]) => {
-        if (typeof value == 'object') {
+        if (typeof value == 'object' && value != null) {
           return [key, deepObjectClone(value)];
         } else {
           return [key, value];
