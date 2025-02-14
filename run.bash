c418ef9777977e531523cdc10d605b0e416a07ae
coolguy284 <coolguy284v64@gmail.com>
2025-02-13 23:39:10 -0500

    update bash cd code more
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- run.bash
+++ run.bash
@@ -1,19 +1,18 @@
 set -e
 
-original_cd=$(pwd)
 code_dir=$(dirname $0)
 
-cd $code_dir
-
-if [[ ! -d node_modules ]]; then
-  npm i --omit=dev --include=optional || (
-    (
-      npm i --omit=dev --omit=optional ||
-      (echo Error installing modules && exit 1)
-    ) && echo Error installing optional modules, but regular modules worked
-  )
-fi
-
-cd $original_cd
+(
+  cd $code_dir
+  
+  if [[ ! -d node_modules ]]; then
+    npm i --omit=dev --include=optional || (
+      (
+        npm i --omit=dev --omit=optional ||
+        (echo Error installing modules && exit 1)
+      ) && echo Error installing optional modules, but regular modules worked
+    )
+  fi
+)
 
 node $code_dir $*
