e8b84b1dcab854a4197916d064c772bdad95e655
coolguy284 <coolguy284v64@gmail.com>
2025-02-13 23:32:17 -0500

    fix bash file npm install

--- run.bash
+++ run.bash
@@ -3,8 +3,12 @@ set -e
 cd $(dirname $0)
 
 if [[ ! -d node_modules ]]; then
-  npm i --omit=dev --include=optional
-  #npm i --omit=dev --omit=optional
+  npm i --omit=dev --include=optional || (
+    (
+      npm i --omit=dev --omit=optional ||
+      (echo Error installing modules && exit 1)
+    ) && echo Error installing optional modules, but regular modules worked
+  )
 fi
 
 node . $*
