1cfa3df40b1218f12e28ceb3f898a06d194a8141
coolguy284 <coolguy284v64@gmail.com>
2025-02-17 00:07:05 -0500

    start making file attribute getter func
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- subpkgs/hb_native_fs/main.cpp
+++ subpkgs/hb_native_fs/main.cpp
@@ -2,15 +2,26 @@
 
 // https://nodejs.org/docs/latest/api/n-api.html#usage
 
-napi_value create_addon(napi_env env) {
+napi_value getItemAttributes(napi_env env, napi_callback_info info) {
   napi_value result;
-  NAPI_CALL_RETURN(env, napi_create_object((env), &result));
+  NAPI_CALL_RETURN(env, napi_create_object(env, &result));
   
-  napi_value test;
-  NAPI_CALL_RETURN(env, napi_create_string_utf8((env), "test_string", NAPI_AUTO_LENGTH, &test));
-  NAPI_CALL_RETURN(env, napi_set_named_property(env, result, "test", test));
+  napi_value readonlyObj;
+  NAPI_CALL_RETURN(env, napi_get_boolean(env, false, &readonlyObj));
+  NAPI_CALL_RETURN(env, napi_set_named_property(env, result, "readonly", readonlyObj));
   
   return result;
+}
+
+napi_value create_addon(napi_env env) {
+  napi_value exports;
+  NAPI_CALL_RETURN(env, napi_create_object(env, &exports));
+  
+  napi_value getItemAttributesObj;
+  NAPI_CALL_RETURN(env, napi_create_function(env, "getItemAttributes", NAPI_AUTO_LENGTH, getItemAttributes, nullptr, &getItemAttributesObj));
+  napi_set_named_property(env, exports, "getItemAttributes", getItemAttributesObj);
+  
+  return exports;
 }
 
 NAPI_MODULE_INIT(/* napi_env env, napi_value exports */) {
