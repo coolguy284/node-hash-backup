97d273ff741687d97b78d430af02af71cfae0426
coolguy284 <coolguy284v64@gmail.com>
2025-02-16 23:34:38 -0500

    implement working native framework
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- subpkgs/hb_native_fs/main.mjs
+++ subpkgs/hb_native_fs/main.mjs
@@ -1,1 +1,4 @@
-import {} from './build/Release/hb_native_fs.node';
+import { createRequire } from 'node:module';
+
+// https://medium.com/the-node-js-collection/how-to-import-native-modules-using-the-new-es6-module-syntax-426ca3c44bed
+const hbNativeFs = createRequire(import.meta.url)('./build/Release/hb_native_fs.node');
