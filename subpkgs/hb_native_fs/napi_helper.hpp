0214493004c84ad11c2a86a850dd417f9f391824
coolguy284 <coolguy284v64@gmail.com>
2026-05-09 23:54:33 -0400

    move native-fs module out of node-hash-backup

--- subpkgs/hb_native_fs/napi_helper.hpp
+++
@@ -1,8 +0,0 @@
-#define NAPI_VERSION 9
-#define NAPI_EXPERIMENTAL
-#include <node_api.h>
-
-bool process_napi_call(napi_env env, napi_status call_result);
-
-// https://nodejs.org/docs/latest/api/n-api.html#usage
-#define NAPI_CALL_RETURN(env, call) if (!process_napi_call((env), (call))) { return nullptr; }
